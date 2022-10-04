61b4bce67380753e7c362d7597fe744372790fb3
sacha <gorse.sacha@gmail.com>
2022-10-04 20:08:49 +0200

    [LABYRINTH-4] : Ajout de la gestion de plusieurs fenêtre (pour le futur)

--- core/src/com/mygdx/labyrinth/Labyrinth.java
+++ core/src/com/mygdx/labyrinth/Labyrinth.java
@@ -1,7 +1,9 @@
 package com.mygdx.labyrinth;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -10,62 +12,27 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Labyrinth extends ApplicationAdapter {
+public class Labyrinth extends Game {
 
-	private OrthographicCamera camera;
-	// Permet de rendre les images ou les textures
-	private SpriteBatch batch;
-	// Nombre de colonne et de ligne dans l'image de base
-	private static final int FRAME_COLS = 3, FRAME_ROWS = 4;
-	// Héros animé
-	private Animation<TextureRegion> walkAnimation;
-	private Texture walkSheet;
-
-	// Variable pour tracker le temps écoulé pour l'animation
-	private float stateTime;
+	private SpriteBatch batchPrincipal;
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		camera = new OrthographicCamera(220,200);
-		camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
-		camera.update();
-
-		walkSheet = new Texture(Gdx.files.internal("heros.png"));
-		TextureRegion[][] tmp = TextureRegion.split(walkSheet,
-				walkSheet.getWidth() / FRAME_COLS,
-				walkSheet.getHeight() / FRAME_ROWS);
-
-		TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
-		int index = 0;
-		for (int i = 0; i < FRAME_ROWS; i++) {
-			for (int j = 0; j < FRAME_COLS; j++) {
-				walkFrames[index++] = tmp[i][j];
-			}
-		}
-
-		walkAnimation = new Animation<TextureRegion>(0.1f, walkFrames);
-
-		stateTime = 0f;
+		this.batchPrincipal = new SpriteBatch();
+		this.setScreen(new GameScreen(this));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(0, 0, 0, 1);
-		camera.update();
-
-		stateTime += Gdx.graphics.getDeltaTime();
-
-		TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		batch.draw(currentFrame, 50, 50);
-		batch.end();
+		super.render();
 	}
-	
+
 	@Override
 	public void dispose () {
-		batch.dispose();
-		walkSheet.dispose();
+		batchPrincipal.dispose();
+	}
+
+	public SpriteBatch getBatchPrincipal() {
+		return this.batchPrincipal;
 	}
 }
